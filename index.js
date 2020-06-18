f1cfb350beaff155688b39c5069d0947b4ff3024
Sigita Senkiene <senkiene.sigita@gmail.com>
2020-06-18 13:26:30 +0300

    Generate maze with for loop

--- index.js
+++ index.js
@@ -33,3 +33,15 @@ const walls = [
 ]
 
 World.add(world, walls);
+
+// Maze generation
+
+const grid = [];
+
+for(let i = 0; i < 3; i++) {
+    grid.push([])
+    for(let j = 0; j < 3; j++) {
+        grid[i].push(false);
+    }  
+}
+console.log(grid);
