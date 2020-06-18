aab8e7881999d550e06d6fc0355c193b1e849064
Sigita Senkiene <senkiene.sigita@gmail.com>
2020-06-18 11:47:40 +0300

    Add random shapes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
+
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
@@ -26,4 +31,22 @@ const walls = [
 ]
 
 World.add(world, walls);
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Random shapes
+for(let i = 0; i < 50; i++) {
+    if(Math.random() > 0.5) {
+        World.add(
+            world, 
+            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+        );
+    } else {
+        World.add(
+            world,
+            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+                render: {
+                    fillStyle: 'deeppink'
+                }
+            })
+        );
+    }
+    
+}
