4c141bd67821298b080cad0a281d325f5374870c
Sigita Senkiene <senkiene.sigita@gmail.com>
2020-06-18 10:34:11 +0300

    Add shape to canvas

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+    isStatic: true
+});
+
+World.add(world, shape);
